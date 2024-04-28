03d82c6feed8a8bb7ae32b258e51ce728349ed7b
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-28 08:58:58 +0200

    Stage 1/3

--- Chalkboard Printer/task/task.js
+++ Chalkboard Printer/task/task.js
@@ -1,16 +1,15 @@
-const input = require('sync-input');
-
+'use strict';
 
 function solution() {
-    let name = input("Enter your name: ");
-    let surname = input("Enter your surname: ");
-    let message = input("Enter your message: ");
-    let repeats = input("Enter number of repeats: ");
+    let name = 'Bart';
+    let surname = 'Simpson';
+    let message = 'I will not skateboard in the halls.';
 
-    for (let i = 0; i < repeats; i++) {
-        console.log(`This is ${name} ${surname} and ${message}`);
-    }
-
+    console.log(`This is ${name} ${surname} and ${message}`);
+    console.log(`This is ${name} ${surname} and ${message}`);
+    console.log(`This is ${name} ${surname} and ${message}`);
+    console.log(`This is ${name} ${surname} and ${message}`);
+    console.log(`This is ${name} ${surname} and ${message}`);
 }
 
 solution();
